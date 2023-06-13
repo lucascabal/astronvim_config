723e282072fe7b4f3dfe67ba5fde69d3120564f5
lukz <lucassuarez@neosystems.es>
2023-06-13 07:44:33 +0200

    tmp

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,5 +6,20 @@ return {
 
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.mini-surround" },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "C-Sa", -- Add surrounding in Normal and Visual modes
+        delete = "C-Sd", -- Delete surrounding
+        find = "C-Sf", -- Find surrounding (to the right)
+        find_left = "C-SF", -- Find surrounding (to the left)
+        highlight = "C-Sh", -- Highlight surrounding
+        replace = "C-Sr", -- Replace surrounding
+        update_n_lines = "C-Sn", -- Update `n_lines`
+      },
+    },
+  },
+
   -- { import = "astrocommunity.completion.copilot-lua-c mp" },
 }
